9a856e6f513994c5aeb24a02cb4219d20cbd6957
proskuryakov_e_d <proskur.egor@yandex.ru>
2021-08-24 12:36:25 +0300

    Initial commit

---
+++ src/test/java/ru/proskuryakov/testcbrcursonadapter/TestCbrCursOnAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.proskuryakov.testcbrcursonadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCbrCursOnAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
