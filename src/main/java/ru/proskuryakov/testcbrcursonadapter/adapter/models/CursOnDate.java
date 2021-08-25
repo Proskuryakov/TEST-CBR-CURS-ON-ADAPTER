f4b46fdd00732d525796140c49d505f6ff9de55d
proskuryakov_e_d <proskur.egor@yandex.ru>
2021-08-25 16:07:44 +0300

    Change model to new backend-model

--- src/main/java/ru/proskuryakov/testcbrcursonadapter/adapter/models/CursOnDate.java
+++ src/main/java/ru/proskuryakov/testcbrcursonadapter/adapter/models/CursOnDate.java
@@ -1,17 +1,14 @@
 package ru.proskuryakov.testcbrcursonadapter.adapter.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 @Data
 public class CursOnDate {
 
     private String code;
     private BigDecimal curs;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private Date date;
+    private String date;
 
 }
