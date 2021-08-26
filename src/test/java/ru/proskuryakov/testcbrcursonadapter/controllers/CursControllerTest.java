62b4d4ea064c2181d97cd83d0f7b936a6a3830db
proskuryakov_e_d <proskur.egor@yandex.ru>
2021-08-26 13:20:35 +0300

    Add test for rabbit-logging

--- src/test/java/ru/proskuryakov/testcbrcursonadapter/controllers/CursControllerTest.java
+++ src/test/java/ru/proskuryakov/testcbrcursonadapter/controllers/CursControllerTest.java
@@ -6,15 +6,19 @@ import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import io.restassured.module.mockmvc.config.RestAssuredMockMvcConfig;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.proskuryakov.testcbrcursonadapter.adapter.models.CodeWithDates;
 import ru.proskuryakov.testcbrcursonadapter.adapter.models.CursOnDate;
 import ru.proskuryakov.testcbrcursonadapter.cbr.CbrService;
+import ru.proskuryakov.testcbrcursonadapter.rabbit.LoggingMessage;
+import ru.proskuryakov.testcbrcursonadapter.rabbit.RabbitLoggingListener;
 import ru.proskuryakov.testcbrcursonadapter.utils.TestUtils;
 import ru.proskuryakov.testcbrcursondateadapter.cbr.wsdl.ValuteData;
 
@@ -27,16 +31,21 @@ import static org.hamcrest.CoreMatchers.*;
 import static io.restassured.RestAssured.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class CursControllerTest {
 
     @Autowired
     private CbrService cbrService;
 
+    @Autowired
+    private RabbitLoggingListener loggingListener;
+
     @Value("${adapter.uri}")
     private String url;
 
     @Test
-    public void getCursByCodeTest() {
+    @DisplayName("GetCursByCodeTest")
+    public void test_0001() {
         String code = "USD";
         GregorianCalendar date = new GregorianCalendar();
 
@@ -60,7 +69,16 @@ public class CursControllerTest {
     }
 
     @Test
-    public void getCursByCodeAndDateTest() {
+    @DisplayName("Rabbit logging getCursByCode test")
+    public void test_0002() {
+        LoggingMessage loggingMessage = loggingListener.getMessage();
+        assertThat(loggingMessage, is(notNullValue()));
+        assertThat(loggingMessage.getAction(), Matchers.equalTo("getCursByCode"));
+    }
+
+    @Test
+    @DisplayName("getCursByCodeAndDateTest")
+    public void test_0003() {
         String code = "USD";
         GregorianCalendar calendar = new GregorianCalendar(2020, Calendar.FEBRUARY, 20);
         String strDate = TestUtils.dateToString(calendar);
@@ -85,7 +103,16 @@ public class CursControllerTest {
     }
 
     @Test
-    public void getCursByDates() {
+    @DisplayName("Rabbit logging getCursByCodeAndDate test")
+    public void test_0004() {
+        LoggingMessage loggingMessage = loggingListener.getMessage();
+        assertThat(loggingMessage, is(notNullValue()));
+        assertThat(loggingMessage.getAction(), Matchers.equalTo("getCursByCodeAndDate"));
+    }
+
+    @Test
+    @DisplayName("getCursByDates")
+    public void test_0005() {
         String code = "USD";
         List<String> dates = List.of("2001-01-17", "2021-01-17");
 
@@ -119,5 +146,12 @@ public class CursControllerTest {
         assertThat(curses[1].getCode(), Matchers.equalTo(second.getVchCode()));
     }
 
+    @Test
+    @DisplayName("Rabbit logging getCursByDates test")
+    public void test_0006() {
+        LoggingMessage loggingMessage = loggingListener.getMessage();
+        assertThat(loggingMessage, is(notNullValue()));
+        assertThat(loggingMessage.getAction(), Matchers.equalTo("getCursByDates"));
+    }
 
 }
